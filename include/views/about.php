f303a32d62dba8a42dfa74c54f083712b29aeb32
idaahlin <ahlinida95@gmail.com>
2016-05-24 21:02:03 +0200

    Uppdaterat profile.php och about-php
    Lagt divarna horisontellt
    Lagt till text/beskrivning
    Lagt till en css-fil för profilsidan

--- include/views/about.php
+++ include/views/about.php
@@ -1,24 +1,24 @@
 <h1>Hur fungerar det?</h1>
 
 <div class='about'>
-  <div class='step-box'>
+  <div class='container'>
     <img src="http://placehold.it/150x150" alt='steg1'>
-    <h3>Rubrik</h3>
-    <p>Text</p>
+    <h3>Registrera dig</h3>
+    <p>Registrera dig hos oss<br></br>för att komma igång direkt.</p>
   </div>
-  <div class='step-box'>
+  <div class='container'>
     <img src="http://placehold.it/150x150" alt='steg2'>
-    <h3>Rubrik</h3>
-    <p>Text</p>
+    <h3>Gör en beställning</h3>
+    <p>Berätta för oss vad du vill<br></br>skicka eller skicka efter.</p>
   </div>
-  <div class='step-box'>
+  <div class='container'>
     <img src="http://placehold.it/150x150" alt='steg3'>
-    <h3>Rubrik</h3>
-    <p>Text</p>
+    <h3>Vi skickar en drönare</h3>
+    <p>En drönare med produkten<br></br>skickas iväg.</p>
   </div>
-  <div class='step-box'>
+  <div class='container'>
     <img src="http://placehold.it/150x150" alt='steg4'>
-    <h3>Rubrik</h3>
-    <p>Text</p>
+    <h3>Varan anländer</h3>
+    <p>Varan landar, snabbt och enkelt,<br></br>på önskad plats.</p>
   </div>
 </div>
---
+++ include/views/profile.php
@@ -0,0 +1,9 @@
+<h1>Min profil</h1>
+<div class='container' id="profilePic">
+  <img src="http://www.netdoktorpro.se/images/img/no_avatar.gif" alt='steg1'>
+</div>
+
+<form>
+  Email: <input type="text" name="Namn: " value="Sven Svensson" readonly><br>
+  Country: <input type="text" name="E-mail: " value="sven.son@gmail.com" readonly><br>
+</form>
