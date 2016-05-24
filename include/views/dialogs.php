42e21bc1c6f9849951238982e4145fe120d91a00
idaahlin <ahlinida95@gmail.com>
2016-05-24 14:48:14 +0200

    Merge branch 'master' into dialogs
    # Conflicts:
    #	include/views/dialogs.php
    #	include/views/menu.php

--- include/views/dialogs.php
+++ include/views/dialogs.php
@@ -1,15 +1,29 @@
 <dialog id='loginDialog'>
-  <form method="post" action="include/models/validator.php" name='loginForm'>
-    E-mail: <input type="email" name="email"/><br><br>
-    Lösenord: <input type="password" name="password"/><br><br>
-    <input type="submit" name="loginInDialog" value="Logga in"/>
+  <form name='loginForm'>
+    <label for='email'>Email</label>
+    <input type='text' name='email' placeholder='Email'>
+
+    <label for='password'>Lösenord</label>
+    <input type='password' name='password' placeholder='Lösenord'>
+
+    <input type='submit' name='submitRegister' class='button' value='Registrera'>
+    <button type='button' id='hideModal' class='redBtn'>Avbryt</button>
   </form>
 </dialog>
 
 <dialog id='registerDialog'>
-  <form method="post" action="include/models/validator.php" name='registerForm'>
-    E-mail: <input type="email" name="email"/><br><br>
-    Lösenord: <input type="password" name="password"/><br><br>
-    <input type="submit" name="regInDialog" value="Registrera dig"/>
+  <form name='registerForm' method='POST' action='register.php'>
+
+    <label for='name'>Namn</label>
+    <input type='text' name='name' placeholder='Namn'>
+
+    <label for='email'>Email</label>
+    <input type='text' name='email' placeholder='Email'>
+
+    <label for='password'>Lösenord</label>
+    <input type='password' name='password' placeholder='Lösenord'>
+
+    <input type='submit' name='submitRegister' class='button' value='Registrera'>
+    <button type='button' id='hideModal' class='redBtn'>Avbryt</button>
   </form>
 </dialog>
--- include/views/menu.php
+++ include/views/menu.php
@@ -3,13 +3,13 @@
     // echo $_SERVER['PHP_SELF'];
     if(isset($_SESSION['email'])){
       echo "<div>
-              <button id='logout' type='button'>Logga ut</button>
+              <button id='logoutBtn' type='button'>Logga ut</button>
             </div>";
     }
     else{
       echo "<div>
-              <button id='login' type='button' onclick='logIn()'>Logga in</button>
-              <button id='register' type='button' onclick='reg()'>Registrera</button>
+              <button id='loginBtn' type='button' onclick='logIn()'>Logga in</button>
+              <button id='registerBtn' type='button' onclick='reg()'>Registrera</button>
             </div>";
     }
    ?>
