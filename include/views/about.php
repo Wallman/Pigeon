5862d77c4d586fa48005ecdb542e6b2d28981b69
Wallman <daniel.wallman94@gmail.com>
2016-05-30 11:49:56 +0200

    Text i about

--- include/views/about.php
+++ include/views/about.php
@@ -2,7 +2,7 @@
   <div>
     <h1>Vad är Pigeon?</h1>
 
-    <p>Pigeon är en tjänst där du enkelt kan beställa transport av varor med drönare.</p>
+    <p>Pigeon är en e-tjänst som kopplar ihop kunder med transportörer och <br/> därmed möjliggör enkel beställning av varutransport med drönare.</p>
   </div>
   <div>
     <h1>Hur fungerar det?</h1>
@@ -10,22 +10,22 @@
     <div class='container'>
       <img class='icon' src='assets/img/about1.png' alt='about1'>
       <h3>Registrera dig</h3>
-      <p>Registrera dig hos oss<br></br>för att komma igång direkt.</p>
+      <p>Registrera dig hos oss<br/>för att komma igång direkt.</p>
     </div>
     <div class='container'>
       <img class='icon' src='assets/img/about2.png' alt='about2'>
       <h3>Gör en beställning</h3>
-      <p>Berätta för oss vad du vill<br></br>skicka eller skicka efter.</p>
+      <p>Berätta för oss vad du vill<br/>skicka eller skicka efter.</p>
     </div>
     <div class='container'>
       <img class='icon' src='assets/img/about3.png' alt='about3'>
       <h3>Vi skickar en drönare</h3>
-      <p>En drönare med produkten<br></br>skickas iväg.</p>
+      <p>En drönare med produkten<br/>skickas iväg.</p>
     </div>
     <div class='container'>
       <img class='icon' src='assets/img/about4.png' alt='about4'>
       <h3>Varan anländer</h3>
-      <p>Varan landar, snabbt och enkelt,<br></br>på önskad plats.</p>
+      <p>Varan landar, snabbt och enkelt,<br/>på önskad plats.</p>
     </div>
   </div>
 </div>
